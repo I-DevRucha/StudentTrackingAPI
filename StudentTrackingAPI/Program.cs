f9e5a3f72e9e2a832f6d9ffeb7bb854893a5499d
runali40 <runali.devit@gmail.com>
2025-07-10 14:44:46 +0530

    login

--- StudentTrackingAPI/Program.cs
+++ StudentTrackingAPI/Program.cs
@@ -1,3 +1,14 @@
+
+using Microsoft.IdentityModel.Tokens;
+using StudentTrackingAPI.Core.Repository;
+using StudentTrackingAPI.DataAccess.Context;
+using StudentTrackingAPI.Services.ApiServices;
+using StudentTrackingAPI.Services.Interfaces;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +17,64 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Add session state service
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    // options.Cookie.HttpOnly = true;
+    options.Cookie.Name = "ephr";
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddCors(o => o.AddPolicy("default", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowOrigin",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:44351", "http://localhost:3000", "https://eohc.in")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+});
+
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+//{
+//    options.RequireHttpsMetadata = false;
+//    options.SaveToken = true;
+//    options.TokenValidationParameters = new TokenValidationParameters()
+//    {
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidAudience = builder.Configuration["Jwt:Audience"],
+//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+//    };
+//});
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<DatabaseContext>(opts => opts.UseSqlServer(builder.Configuration["ConnectionStrings:dev"]));
+}
+builder.Services.AddDbContext<DatabaseContext>(opts => opts.UseSqlServer(builder.Configuration["ConnectionStrings:prod"]));
+
+builder.Services.AddScoped<IAuthService, AuthService>().AddScoped<AuthRepository>();
+
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 
 var app = builder.Build();
 
@@ -18,9 +87,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseStaticFiles(); // Required to serve files from wwwroot
+
+
 app.UseCors(builder =>
 {
     builder
@@ -29,5 +102,6 @@ app.UseCors(builder =>
     .AllowAnyHeader();
 });
 app.UseCors("AllowOrigin");
+
 
 app.Run();
